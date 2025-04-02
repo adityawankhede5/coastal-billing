185407dafe68b4ab374da0e2382c7f1c5698e9ef
aditya_w <aditya.w@snaptrude.com>
2025-04-03 01:38:16 +0530

    Add loading state handling in Home component with LoadingSkeleton

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 "use client"
 import useOrdersStore from "@/zustand/store";
 import OrdersList from "@/components/OrdersList";
+import LoadingSkeleton from "@/components/Skeletons/LoadingSkeleton";
 export default function Home() {
-  const { orders } = useOrdersStore();
+  const { orders, loading } = useOrdersStore();
+  if (loading) return <LoadingSkeleton />;
   return (
     <div className="grid grid-cols-1 gap-2">
       <OrdersList orders={orders} />
