e812ce889f67a3bcebdcef0c99de422efe067b59
aditya_w <aditya.w@snaptrude.com>
2025-05-07 22:50:08 +0530

    Update Button component to include type and onClick props for enhanced functionality and flexibility in button usage.

--- components/UI/Button.tsx
+++ components/UI/Button.tsx
@@ -1,6 +1,6 @@
-export default function Button({ children, className, secondary = false, ...props }: { children: React.ReactNode, className?: string, secondary?: boolean, [key: string]: any }) {
+export default function Button({ children, className, secondary = false, type = "button", onClick }: { children: React.ReactNode, className?: string, secondary?: boolean, type?: "button" | "submit" | "reset", onClick?: () => void }) {
   return (
-    <button className={` px-4 py-2 rounded-md ${className} ${secondary ? "bg-transparent border border-indigo-600 text-indigo-600" : "text-white bg-indigo-600"}`} {...props}>
+    <button type={type} className={` px-4 py-2 rounded-md ${className} ${secondary ? "bg-transparent border border-indigo-600 text-indigo-600" : "text-white bg-indigo-600"}`} onClick={onClick}>
       {children}
     </button>
   )
