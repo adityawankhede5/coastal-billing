250f3e66ca7a59efcd7b25b5242fd78d2849476f
aditya_w <aditya.w@snaptrude.com>
2025-05-15 00:15:39 +0530

    Update CartButton and NewOrderButton components to increase button size and icon dimensions for improved usability and visual consistency.

--- components/CartButton.tsx
+++ components/CartButton.tsx
@@ -2,8 +2,8 @@ import CartIcon from "@/assets/icons/CartIcon";
 
 export default function CartButton({ onClick }: { onClick: () => void }) {
   return (
-    <span onClick={onClick} className="fixed bottom-6 right-4 w-12 h-12 bg-blue-600 rounded-full flex justify-center items-center">
-      <CartIcon className="w-7 h-7 text-white" />
+    <span onClick={onClick} className="fixed bottom-6 right-4 w-15 h-15 bg-blue-600 rounded-full flex justify-center items-center">
+      <CartIcon className="w-8 h-8 text-white" />
     </span>
   );
 }
