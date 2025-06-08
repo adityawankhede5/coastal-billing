a14eb43f91cb573fb0d44fb5c3410ba043529044
aditya_w <aditya.w@snaptrude.com>
2025-06-08 18:07:53 +0530

    fix: Add to cart on click on the card itself.

--- components/MenuItemCard.tsx
+++ components/MenuItemCard.tsx
@@ -6,8 +6,13 @@ import { MENU_ITEM } from "@/constants/types";
 
 export default function MenuItemCard({ item, quantity = 0, query, handleUpdateCart }: { item: MENU_ITEM, quantity: number, query: string, handleUpdateCart: (itemId: string, quantity: number) => void }) {
   const Icon = MENU_CATEGORY_ICONS[item.type];
+  const onPlusMinusClick = (event: React.MouseEvent<HTMLSpanElement>, quantity: number = 1) => {
+    event.preventDefault();
+    event.stopPropagation();
+    handleUpdateCart(item.id, quantity);
+  }
   return (
-    <div className="relative bg-white rounded-xl grid grid-rows-[auto_1fr_auto] shadow-md overflow-hidden hover:shadow-xl transition-shadow duration-300 w-full min-h-56 max-h-56 p-4">
+    <div className="relative bg-white rounded-xl grid grid-rows-[auto_1fr_auto] shadow-md overflow-hidden hover:shadow-xl transition-shadow duration-300 w-full min-h-56 max-h-56 p-4" onClick={(e) => onPlusMinusClick(e, 1)}>
       <Icon className="absolute top-0 right-0 w-24 h-24 text-indigo-200/15" />
       <div>
         <span className="text-lg font-bold text-gray-900"><HighlightText text={item.name} query={query} /></span>
@@ -19,11 +24,11 @@ export default function MenuItemCard({ item, quantity = 0, query, handleUpdateCa
       </div>
 
       <div className="flex items-center gap-1 h-8">
-        <span style={{ opacity: quantity <= 0 ? 0.3 : 1 }} className="bg-gray-900 rounded-full w-8 h-8 text-white flex items-center justify-center" onClick={() => handleUpdateCart(item.id, -1)}><MinusIcon /></span>
+        <span style={{ opacity: quantity <= 0 ? 0.3 : 1 }} className="bg-gray-900 rounded-full w-8 h-8 text-white flex items-center justify-center" onClick={(e) => onPlusMinusClick(e, -1)}><MinusIcon /></span>
         <span className="text-lg font-bold text-indigo-600 text-center w-6">
           {quantity}
         </span>
-        <span className="bg-gray-900 rounded-full w-8 h-8 text-white flex items-center justify-center" onClick={() => handleUpdateCart(item.id, 1)}><PlusIcon /></span>
+        <span className="bg-gray-900 rounded-full w-8 h-8 text-white flex items-center justify-center" onClick={(e) => onPlusMinusClick(e, 1)}><PlusIcon /></span>
         <span className="text-lg font-bold text-indigo-600 text-right flex-1">
           &#8377;{quantity * item.price}
         </span>
