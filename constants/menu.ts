7945974891565ac0deb365ee188a2466e4f4ff5a
aditya_w <aditya.w@snaptrude.com>
2025-06-09 20:03:58 +0530

    Add Extra Cheese to SIDES menu category with updated item details.

--- constants/menu.ts
+++ constants/menu.ts
@@ -57,6 +57,7 @@ const GOODMORNINGS: Record<string, MENU_ITEM> = {
 
 const SIDES: Record<string, MENU_ITEM> = {
   "yutrgh": { "id": "yutrgh", "name": "Fries", "description": "Crispy fries", "price": 65, "type": MENU_CATEGORY.SIDES },
+  "qztrgh": { "id": "qztrgh", "name": "Extra Cheese", "description": "Extra Cheese", "price": 10, "type": MENU_CATEGORY.SIDES },
   "cmert": { "id": "cmert", "name": "Choco Meltwich", "description": "Coastal Special", "price": 90, "type": MENU_CATEGORY.SIDES },
   "wbrtij": { "id": "wbrtij", "name": "Water Bottle 0.5L", "description": "500ml", "price": 10, "type": MENU_CATEGORY.SIDES },
   "wbytrf": { "id": "wbytrf", "name": "Water Bottle 1L", "description": "1L", "price": 20, "type": MENU_CATEGORY.SIDES },
